1bbb154fb015f6835b3c48e61da7a790b00d98e9
gabrimat1 <gabrimat1@protonmail.ch>
2023-05-26 18:27:37 +0200

    Updated controller.py
    Added automatic plotter for errors on the x, y and d.

--- controller.py
+++ controller.py
@@ -3,6 +3,8 @@ import cv2
 from djitellopy import Tello
 from simple_pid import PID
 from obstacles_detector import ObstaclesDetector
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 class Controller:
     """
@@ -11,12 +13,11 @@ class Controller:
     CASCADE = "models/cascade.xml", "Cascade"
     FASTER_RCNN = "models/fasterrcnn.pth", "Faster R-CNN"
     SSD_LITE = "models/ssdlite.pth", "SSDLite"
+    
     def __init__(self, tello: Tello, model_path: str, model_type: str = "SDDLite") -> None:
         self.tello = tello
         self.frame_to_stream = None
-        self.land_signal_detected = False
-        print(model_path)
-        print(model_type)
+
         self.obstacles_detector = ObstaclesDetector(model_path, model_type)
 
         self.frame_width = 360
@@ -25,6 +26,8 @@ class Controller:
         self.pid_x = PID(-0.3, -0.01, -0.1, setpoint=self.frame_width // 2)
         self.pid_y = PID(0.3, 0.01, 0.1, setpoint=self.frame_height // 2 - 30)
         self.pid_d = PID(0.0003, 0, 0, setpoint=self.frame_height*self.frame_width)
+
+        self._initialize_plot()
 
     def update(self) -> None:
         """
@@ -65,3 +68,71 @@ class Controller:
         forward_backward_velocity = int(self.pid_d(max_area))
 
         self.tello.send_rc_control(left_right_velocity, forward_backward_velocity, up_down_velocity, 0)
+
+    def _initialize_plot(self):
+        """
+        Initializer of the subplots for the real-time plot.
+        """
+        self.error_history_x = []
+        self.error_history_y = []
+        self.error_history_d = []
+        self.fig, axs = plt.subplots(3, 1, figsize=(20,18))
+        self.fig.subplots_adjust(hspace=0.5)
+        self.fig.set_dpi(70)
+
+        x = np.linspace(0,1000,1000)
+        y = np.zeros((1000))
+        variables = ["x", "y", "d"]
+        for i in range(3):
+            axs[i].set_xlim(0, 100)
+            axs[i].set_ylim(-200, 200)
+            axs[i].set_xlabel('Time (s)')
+            axs[i].set_ylabel(f'Error on {variables[i]}')
+            axs[i].set_title(f'Real-Time Error Plot for {variables[i]}')
+            axs[i].plot(x,  y, linestyle='--', color='grey', label='Dotted Line')
+
+        self.line0, = axs[0].plot([], [], lw=2, color='red')
+        self.line1, = axs[1].plot([], [], lw=2, color='blue')
+        self.line2, = axs[2].plot([], [], lw=2, color='green')
+
+        self.ani = FuncAnimation(self.fig, self._ani_func, init_func=self._ani_init_func, frames=10000, interval=500, blit=True)
+        plt.show()
+
+
+    # initialization function: plot the background of each frame
+    def _ani_init_func(self):
+        """
+        Initializer for the FuncAnimation object.
+        """
+        self.line0.set_data([], [])
+        self.line1.set_data([], [])
+        self.line2.set_data([], [])
+        return self.line0, self.line1, self.line2,
+
+    def _ani_func(self, i):
+        """
+        Function called each frame by FuncAnimation object.
+        """
+        error_x = self.pid_x._last_error if self.pid_x._last_error else 0
+        error_y = self.pid_y._last_error if self.pid_y._last_error else 0
+        error_d = self.pid_d._last_error if self.pid_d._last_error else 0
+
+        ms=i/2
+        self.times.append(ms)
+        self.error_history_x.append(error_x)
+        self.error_history_y.append(error_y)
+        self.error_history_d.append(error_d)
+
+        x0 = self.times
+        y0 = self.error_history_x
+        self.line0.set_data(x0, y0)
+
+        x1 = self.times
+        y1 = self.error_history_y
+        self.line1.set_data(x1, y1)
+
+        x2 = self.times
+        y2 = self.error_history_d
+        self.line2.set_data(x2, y2)
+
+        return self.line0, self.line1, self.line2,
